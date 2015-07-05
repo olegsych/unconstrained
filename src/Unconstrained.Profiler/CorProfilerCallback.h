721492d48175ff8b4c92c06110fec399a03074f4
Oleg Sych <oleg_sych@hotmail.com>
2015-07-05 08:21:06 -0700

    - Suppress compiler warning about typedef being ignored in corhdr.h

--- src/Unconstrained.Profiler/CorProfilerCallback.h
+++ src/Unconstrained.Profiler/CorProfilerCallback.h
@@ -1,6 +1,9 @@
 #pragma once
 
+// Suppress warnings about typedef being ignored in corhdr.h
+#pragma warning(disable: 4091)
 #include <cor.h>
+#pragma warning(default: 4091)
 #include <corprof.h>
 #include "dllexport.h"
 
--- src/Unconstrained.Profiler/stdafx.h
+++ src/Unconstrained.Profiler/stdafx.h
@@ -1,4 +1,7 @@
 #pragma once
 
+// Suppress warnings about typedef being ignored in corhdr.h
+#pragma warning(disable: 4091)
 #include <cor.h>
+#pragma warning(default: 4091)
 #include <corprof.h>
